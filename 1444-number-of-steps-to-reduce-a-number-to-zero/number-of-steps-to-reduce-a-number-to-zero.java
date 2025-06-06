983add48d4add00edc681295fc41d8be1df67965
AJAY V <ajayvelanchery@gmail.com>
2025-06-06 23:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (53.71%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -5,16 +5,16 @@
                
             if(num%2==0){
                 num=num/2;
-                i++;
+               
 
             }
 
             else{
                 num=num-1;
-                i++;
+                
             }
 
-          
+          i++;
         }
           return i;
     }
