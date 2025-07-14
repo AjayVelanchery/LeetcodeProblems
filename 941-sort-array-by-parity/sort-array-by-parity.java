8ae0a6483d8c717f7146afdd948016b397a7db68
AJAY V <ajayvelanchery@gmail.com>
2025-07-14 13:11:18 +0530

    Time: 1 ms (51.13%) | Memory: 45 MB (42.04%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,28 +1,25 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
 
+        int arr[]=new int[nums.length];
 
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
+        int index=0;
 
-             stack.push(nums[i]);
+        for(int n:nums){
+
+            if(n%2==0){
+                arr[index++]=n;
             }
         }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2!=0){
-            stack.push(nums[i]);
-        }}
-Integer[] boxed = stack.toArray(new Integer[0]);
 
-       
-        int[] result = new int[boxed.length];
-        for (int i = 0; i < boxed.length; i++) {
-            result[i] = boxed[i]; 
+        for(int n:nums){
 
-    
-
+            if(n%2!=0){
+                arr[index++]=n;
+            }
+        }
         
+
+        return arr;
     }
-        return result;}
 }
