1b66cbd0df38a630dc13232a13d5a57648e34a90
AJAY V <ajayvelanchery@gmail.com>
2025-07-21 13:48:04 +0530

    Time: 3 ms (43.67%) | Memory: 41.7 MB (44.72%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,21 +1,15 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
+
+        Set <Character> st=new HashSet<>();
+        for(char c:sentence.toCharArray()){
+            st.add(c);
+        }
+
+        if(st.size()==26){
+            return true;
+        }
+        return false;
         
-        Set<Character>st=new HashSet<>();
-for(char i='a';i<='z';i++){
-    st.add(i);
-}
-
- for(int i=0;i<sentence.length();i++){
-
-    if(st.contains(sentence.charAt(i))){
-        st.remove(sentence.charAt(i));
-    }
- }
-
- if(st.isEmpty()){
-    return true;
- }
- return false;
     }
 }
