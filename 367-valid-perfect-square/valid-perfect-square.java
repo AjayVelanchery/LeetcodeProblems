ab00d0645b0e63d529367ad23c29cd1674172940
AJAY V <ajayvelanchery@gmail.com>
2025-08-14 13:16:56 +0530

    Time: 1 ms (18.47%) | Memory: 40.5 MB (38.96%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num<2){
+            return true;
+        }
+
+        for(long i=0;i*i<=num;i++){
+
+            if(i*i==num){
+return true;
+
+            }
+        }
+    
+    return false;
+    }
+}
