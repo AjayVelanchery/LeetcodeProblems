84ffef6dc1d72150d813faee65ed75fb38df4a6f
AJAY V <ajayvelanchery@gmail.com>
2025-08-27 13:42:50 +0530

    Time: 1 ms (93.83%) | Memory: 42.1 MB (25.00%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -5,9 +5,7 @@ class Solution {
         for(int j=0;j<word.length();j++){
             if(Character.isUpperCase(word.charAt(j))){
           i++;}
-          else{
-            k++;
-          }
+         
 
             
 
@@ -16,7 +14,7 @@ class Solution {
         if(i==0||i==word.length()){
             return true;
         }
-        if(Character.isUpperCase(word.charAt(0))&&k==word.length()-1){
+        if(i==1&&Character.isUpperCase(word.charAt(0))){
             return true;
         }
         return false;
