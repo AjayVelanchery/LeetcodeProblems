c8353f60b5c4456df35fff448b560cc0c68e63da
AJAY V <ajayvelanchery@gmail.com>
2025-09-01 23:16:13 +0530

    Time: 5 ms (88.82%) | Memory: 45.2 MB (68.48%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,16 +1,21 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
+        int[] charCount = new int[26];
+        for (char c : chars.toCharArray()) {
+            charCount[c - 'a']++;
+        }
+
         int sum = 0;
 
         for (String word : words) {
-            StringBuilder temp = new StringBuilder(chars); // copy of chars for this word
-            boolean canForm = true;
-
+            int[] wordCount = new int[26];
             for (char c : word.toCharArray()) {
-                int index = temp.indexOf(String.valueOf(c));
-                if (index >= 0) {
-                    temp.deleteCharAt(index); // remove used char
-                } else {
+                wordCount[c - 'a']++;
+            }
+
+            boolean canForm = true;
+            for (int i = 0; i < 26; i++) {
+                if (wordCount[i] > charCount[i]) {
                     canForm = false;
                     break;
                 }
