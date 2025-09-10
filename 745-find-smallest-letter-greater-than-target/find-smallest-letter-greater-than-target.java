8e9ef7ad420a7d1d0aa3ab2fe68ec4b5d2a76ae4
AJAY V <ajayvelanchery@gmail.com>
2025-09-10 09:38:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (18.18%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,29 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+
+        int left=0;
+        int right=letters.length-1;
+        if(letters[right]<=target){
+            return letters[0];
+        }
+
+        while(left<=right){
+
+            int mid=left+(right-left)/2;
+
+            // if(target==letters[mid]){
+            //     return letters[mid];
+            // }
+
+             if(letters[mid]<=target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+           
+        }
+         return letters[left];
+    }
+}
