a61da37b55cc9aed5aed50ddf486f4de870dd536
AJAY V <ajayvelanchery@gmail.com>
2025-12-05 16:13:00 +0530

    Time: 3 ms (74.08%) | Memory: 43.4 MB (12.98%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-    Stack<Character> st=new Stack<>();
-        for(char c : s.toCharArray()){
+
+        Stack<Character> st=new Stack<>();
+
+
+        for(char c:s.toCharArray()){
 
             if(c=='('){
                 st.push(')');
             }
-            else if(c=='{')
-            {
+
+            else if(c=='{'){
                 st.push('}');
             }
-            else if(c=='['){
+
+         else   if(c=='['){
                 st.push(']');
             }
+
 
             else if(st.isEmpty()||st.pop()!=c){
                 return false;
             }
-
+        
         
         }
-  
-     return st.isEmpty();
+        return st.isEmpty();
+        
     }
 }
