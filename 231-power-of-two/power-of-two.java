db9576eb2569cf20c1bacee3fab92bc5d6046890
AJAY V <ajayvelanchery@gmail.com>
2026-05-11 22:47:26 +0530

    Time: 1 ms (96.67%) | Memory: 42.1 MB (96.48%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(n==1||n==2){
+            return true;
+        }
+          if(n%2==1){
+            return false;
+          }
+        while(n>1){
+         if(n%2==1){
+            return false;
+          }
+            n=n/2;
+            if(n==2){
+                return true;
+            }
+
+        }
+    
+        return false;
+    }
+}
