7b577886a2fd962466bd711f6dbc77c5d1d72c78
AJAY V <ajayvelanchery@gmail.com>
2026-05-19 00:41:55 +0530

    Time: 3 ms (75.36%) | Memory: 47.5 MB (83.42%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -5,7 +5,7 @@ class Solution {
 
         int carry=0;
      int p=num.length-1;
-        while(p>=0||k>0||carry>0){
+        while(p>=0||k>0){
 
           int numVal = 0;
             if(p>=0){
@@ -20,9 +20,12 @@ class Solution {
 
             p--;
             k=k/10;
-            ans.add(0,digit);
+            ans.add(digit);
         }
-     
+        if (carry>0){
+            ans.add(carry);
+        }
+        Collections.reverse(ans);
         return ans;
     }
 }
